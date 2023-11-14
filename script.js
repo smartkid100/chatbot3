2408028f2fe4760185bc6bba12b8a16addea5f0a
smartkid100 <87696585+smartkid100@users.noreply.github.com>
2023-11-14 18:06:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
